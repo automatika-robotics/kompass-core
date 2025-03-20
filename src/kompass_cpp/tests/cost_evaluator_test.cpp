41b28ba40243a798f678c7c7320e794789546c94
ahr <ahrasheed@gmail.com>
2025-03-20 16:47:27 +0100

    (fix) Combines boost tests into one until stack smashing is resolved with sycl

--- src/kompass_cpp/tests/cost_evaluator_test.cpp
+++ src/kompass_cpp/tests/cost_evaluator_test.cpp
@@ -2,6 +2,7 @@
 #include "datatypes/trajectory.h"
 #include "test.h"
 #include "utils/cost_evaluator.h"
+#include "utils/collision_check.h"
 #include "utils/logger.h"
 #include <cstddef>
 #define BOOST_TEST_MODULE KOMPASS COSTS TESTS
@@ -199,7 +200,7 @@ Trajectory2D run_test(CostEvaluator &costEval, Path::Path &reference_path,
 
 BOOST_FIXTURE_TEST_SUITE(s, TestConfig)
 
-BOOST_AUTO_TEST_CASE(test_ref_path_cost) {
+BOOST_AUTO_TEST_CASE(test_all_costs) {
   // Create timer
   Timer time;
   LOG_INFO("Running Reference Path Cost Test");
@@ -219,63 +220,51 @@ BOOST_AUTO_TEST_CASE(test_ref_path_cost) {
   if (check) {
     LOG_INFO("Test Passed!");
   }
-}
 
-BOOST_AUTO_TEST_CASE(test_goal_cost) {
-  // Create timer
-  Timer time;
   LOG_INFO("Running Goal Cost Test");
   // Generate test trajectory samples
-  TrajectorySamples2D samples =
+  samples =
       generate_ref_path_test_samples(predictionHorizon, timeStep);
 
-  Trajectory2D minimum_cost_traj =
+  minimum_cost_traj =
       run_test(costEval, reference_path, samples, current_segment_index, 0.0,
                1.0, 0.0, 0.0, 0.0);
-  Trajectory2D sample = samples.getIndex(0);
-  bool check = check_sample_equal_result(sample.path, minimum_cost_traj.path);
+  sample = samples.getIndex(0);
+  check = check_sample_equal_result(sample.path, minimum_cost_traj.path);
   BOOST_TEST(check, "Minimum reference path cost trajectory is found but not "
                     "equal to the correct minimum! "
                         << minimum_cost_traj.path.x);
   if (check) {
     LOG_INFO("Test Passed!");
   }
-}
 
-BOOST_AUTO_TEST_CASE(test_smoothness_cost) {
-  // Create timer
-  Timer time;
   LOG_INFO("Running Smoothness Cost Test");
   // Generate test trajectory samples
-  TrajectorySamples2D samples =
+  samples =
       generate_smoothness_test_samples(predictionHorizon, timeStep);
 
-  Trajectory2D minimum_cost_traj =
+  minimum_cost_traj =
       run_test(costEval, reference_path, samples, current_segment_index, 0.0,
                0.0, 0.0, 1.0, 0.0);
-  Trajectory2D sample = samples.getIndex(0);
-  bool check = check_sample_equal_result(sample.path, minimum_cost_traj.path);
+  sample = samples.getIndex(0);
+  check = check_sample_equal_result(sample.path, minimum_cost_traj.path);
   BOOST_TEST(check, "Minimum reference path cost trajectory is found but not "
                     "equal to the correct minimum! "
                         << minimum_cost_traj.path.x);
   if (check) {
     LOG_INFO("Test Passed!");
   }
-}
 
-BOOST_AUTO_TEST_CASE(test_jerk_cost) {
-  // Create timer
-  Timer time;
   LOG_INFO("Running Jerk Cost Test");
   // Generate test trajectory samples
-  TrajectorySamples2D samples =
+  samples =
       generate_smoothness_test_samples(predictionHorizon, timeStep);
 
-  Trajectory2D minimum_cost_traj =
+  minimum_cost_traj =
       run_test(costEval, reference_path, samples, current_segment_index, 0.0,
                0.0, 0.0, 0.0, 1.0);
-  Trajectory2D sample = samples.getIndex(0);
-  bool check = check_sample_equal_result(sample.path, minimum_cost_traj.path);
+  sample = samples.getIndex(0);
+  check = check_sample_equal_result(sample.path, minimum_cost_traj.path);
   BOOST_TEST(check, "Minimum reference path cost trajectory is found but not "
                     "equal to the correct minimum! "
                         << minimum_cost_traj.path.x);
