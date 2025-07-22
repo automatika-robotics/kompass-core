e3feb5bd6edce2d82bb2f07c3e733ed759c6db46
ahr <ahrasheed@gmail.com>
2025-07-22 04:38:17 +0200

    (feature) Adds processing to pointcloud to laserscan for emergency stop

--- src/kompass_cpp/bindings/bindings_gpu.cpp
+++ src/kompass_cpp/bindings/bindings_gpu.cpp
@@ -42,11 +42,24 @@ void bindings_utils_gpu(py::module_ &m) {
       .def(py::init<CollisionChecker::ShapeType, const std::vector<float> &,
                     const Eigen::Vector3f &, const Eigen::Vector4f &,
                     const float, const float, const float,
-                    const std::vector<double> &>(),
+                    const std::vector<double> &, const float, const float,
+                    const float>(),
            py::arg("robot_shape"), py::arg("robot_dimensions"),
            py::arg("sensor_position_body"), py::arg("sensor_rotation_body"),
            py::arg("critical_angle"), py::arg("critical_distance"),
-           py::arg("slowdown_distance"), py::arg("scan_angles"))
-      .def("check", &CriticalZoneCheckerGPU::check, py::arg("ranges"),
-           py::arg("angles"), py::arg("forward"));
+           py::arg("slowdown_distance"), py::arg("scan_angles"),
+           py::arg("max_height"), py::arg("min_height"), py::arg("range_max"))
+
+      .def("check",
+           py::overload_cast<const std::vector<double> &, bool>(
+               &CriticalZoneCheckerGPU::check),
+           py::arg("ranges"), py::arg("forward"))
+
+      .def("check",
+           py::overload_cast<const std::vector<int8_t> &, int, int, int, int,
+                             float, float, float, bool>(
+               &CriticalZoneCheckerGPU::check),
+           py::arg("data"), py::arg("point_step"), py::arg("row_step"),
+           py::arg("height"), py::arg("width"), py::arg("x_offset"),
+           py::arg("y_offset"), py::arg("z_offset"), py::arg("forward"));
 }
--- src/kompass_cpp/kompass_cpp/include/utils/critical_zone_check.h
+++ src/kompass_cpp/kompass_cpp/include/utils/critical_zone_check.h
@@ -24,7 +24,9 @@ public:
                       const Eigen::Vector3f &sensor_position_body,
                       const Eigen::Vector4f &sensor_rotation_body,
                       const float critical_angle, const float critical_distance,
-                      const float slowdown_distance);
+                      const float slowdown_distance,
+                      const std::vector<double> &angles, const float min_height,
+                      const float max_height, const float range_max);
 
   /**
    * @brief Destroy the CriticalZoneChecker object
@@ -35,30 +37,46 @@ public:
   void preset(const std::vector<double> &angles);
 
   /**
-   * @brief Check if the robot is in the slowdown or critical zone
+   * @brief Uses laserscan data to check if the robot is in the slowdown or
+   * critical zone
    *
    * @param ranges    LaserScan ranges
-   * @param angles    LaserScan angles
    * @param forward   True if the robot is moving forward, false otherwise
    * @return    Slowdown factor (0.0 - 1.0) if in the slowdown zone, 0.0 if in
    * the critical zone (stop), 1.0 otherwise
    */
-  float check(const std::vector<double> &ranges,
-              const std::vector<double> &angles, const bool forward);
+  float check(const std::vector<double> &ranges, const bool forward);
+
+  /**
+   * Uses 3D point cloud data to check if robot is in slowdown or critical zone.
+   *
+   * @param data        Flattened point cloud data (int8), typically in XYZ
+   * format.
+   * @param point_step  Number of bytes between each point in the data array.
+   * @param row_step    Number of bytes between each row in the data array.
+   * @param height      Number of rows (height of the point cloud).
+   * @param width       Number of columns (width of the point cloud).
+   * @param x_offset    Offset (in bytes) to the x-coordinate within a point.
+   * @param y_offset    Offset (in bytes) to the y-coordinate within a point.
+   * @param z_offset    Offset (in bytes) to the z-coordinate within a point.
+   * @return            A 2D occupancy grid as an Eigen::MatrixXi.
+   */
+  float check(const std::vector<int8_t> &data, int point_step, int row_step,
+              int height, int width, float x_offset, float y_offset,
+              float z_offset, const bool forward);
 
 protected:
   double robotHeight_{1.0}, robotRadius_;
+  float min_height_, max_height_, range_max_;
   float angle_right_forward_, angle_left_forward_, angle_right_backward_,
       angle_left_backward_;
+  std::vector<float> sin_angles_;
+  std::vector<float> cos_angles_;
   std::vector<size_t> indicies_forward_, indicies_backward_;
-  bool preset_{false};
   float critical_distance_, slowdown_distance_;
 
   Eigen::Isometry3f sensor_tf_body_ =
       Eigen::Isometry3f::Identity(); // Sensor transformation with
                                      // respect to the robot
-
-  void polarConvertLaserScanToBody(std::vector<double> &ranges,
-                                   std::vector<double> &angles);
 };
 } // namespace Kompass
