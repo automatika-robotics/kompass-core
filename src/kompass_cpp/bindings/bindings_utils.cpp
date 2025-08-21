abb196b04b7f4b20d13e628e5fd96e741d783095
mkabtoul <kabtoul.maria@gmail.com>
2025-08-21 13:03:15 +0200

    (feature) Adds binding for PC to scan function for testing

--- src/kompass_cpp/bindings/bindings_utils.cpp
+++ src/kompass_cpp/bindings/bindings_utils.cpp
@@ -1,7 +1,9 @@
 #include "utils/critical_zone_check.h"
+#include "utils/pointcloud.h"
 #include <nanobind/eigen/dense.h>
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/vector.h>
+#include <nanobind/stl/tuple.h>
 
 namespace py = nanobind;
 using namespace Kompass;
@@ -38,6 +40,29 @@ void bindings_utils(py::module_ &m) {
            py::arg("data"), py::arg("point_step"), py::arg("row_step"),
            py::arg("height"), py::arg("width"), py::arg("x_offset"),
            py::arg("y_offset"), py::arg("z_offset"), py::arg("forward"));
+
+  m_utils.def(
+      "pointcloud_to_laserscan_from_raw",
+      [](const std::vector<int8_t> &data, int point_step, int row_step,
+         int height, int width, int x_offset, int y_offset, int z_offset,
+         double max_range, double min_z, double max_z, double angle_step) {
+        std::vector<double> ranges_out;
+        std::vector<double> angles_out;
+
+        // Call the actual function
+        pointCloudToLaserScanFromRaw(data, point_step, row_step, height, width,
+                                     x_offset, y_offset, z_offset, max_range,
+                                     min_z, max_z, angle_step, ranges_out,
+                                     angles_out);
+
+        // Return both vectors to Python
+        return std::make_tuple(ranges_out, angles_out);
+      },
+      py::arg("data"), py::arg("point_step"), py::arg("row_step"),
+      py::arg("height"), py::arg("width"), py::arg("x_offset"),
+      py::arg("y_offset"), py::arg("z_offset"), py::arg("max_range"),
+      py::arg("min_z"), py::arg("max_z"), py::arg("angle_step"),
+      "Converts raw PointCloud2 binary data to laser scan ranges and angles.");
 
 #if GPU
   bindings_utils_gpu(m_utils);
